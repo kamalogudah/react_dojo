c47fb133006dbcd29987cd0c63447c659cf7a566
kamalogudah <mcpaul2058@gmail.com>
2018-10-31 11:55:21 +0300

    Passing props in HOC using functions

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -2,7 +2,8 @@ import React, { PureComponent } from "react";
 import classes from "./App.css";
 import Persons from "../Components/Persons/Persons";
 import Cockpit from "../Components/Cockpit/Cockpit";
-import WithClass from "../hoc/WithClass";
+import withClass from "../hoc/withClass";
+import Aux from "../hoc/Aux";
 class App extends PureComponent {
   constructor(props) {
     super(props);
@@ -106,7 +107,7 @@ class App extends PureComponent {
     }
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button
           onClick={() => {
             this.setState({ showPersons: true });
@@ -122,7 +123,7 @@ class App extends PureComponent {
           appFooter={this.props.footer}
         />
         {persons}
-      </WithClass>
+      </Aux>
     );
   }
 }
